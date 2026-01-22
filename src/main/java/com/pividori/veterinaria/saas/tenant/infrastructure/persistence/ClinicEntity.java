75fc893238532b6aa0dd4a2cfbdc27ded6ade0e8
SantiagoPividori <pividorisantiago@gmail.com>
2026-01-21 21:34:12 -0300

    refactor(clinic): complete ClinicEntity with yours annotations

--- src/main/java/com/pividori/veterinaria/saas/tenant/infrastructure/persistence/ClinicEntity.java
+++ src/main/java/com/pividori/veterinaria/saas/tenant/infrastructure/persistence/ClinicEntity.java
@@ -1,12 +1,11 @@
 package com.pividori.veterinaria.saas.tenant.infrastructure.persistence;
 
-import com.pividori.veterinaria.saas.tenant.domain.*;
-import com.pividori.veterinaria.shared.UserId;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 import java.time.Instant;
-import java.time.ZoneId;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -18,18 +17,38 @@ import java.time.ZoneId;
 @Table(name = "clinics")
 public class ClinicEntity {
 
-    private final ClinicId id;
-    private ClinicName name;
-    private ClinicLegalName legalName;
-    private ClinicCuit cuit;
-    private ClinicEmail email;
-    private ClinicPhoneNumber phoneNumber;
-    private ClinicAddress address;
-    private ClinicStatus status;
-    private PlanType planType;
-    private final UserId ownerId;
-    private ZoneId timeZone;
-    private final Instant createdAt;
+    @Id
+    private UUID id;
+    @Column(name = "name",  nullable = false)
+    private String name;
+    @Column(name = "legal_name", nullable = false)
+    private String legalName;
+    @Column(name = "cuit",  nullable = false, unique = true)
+    private String cuit;
+    @Email
+    @Column(name = "email",   nullable = false)
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "province")
+    private String province;
+    @Column(name = "postal_code")
+    private String postalCode;
+    @Column(name = "status", nullable = false)
+    private String status;
+    @Column(name = "plan_type",  nullable = false)
+    private String planType;
+    @Column(name = "owner_id", nullable = false)
+    private UUID ownerId;
+    @Column(name = "time_zone")
+    private String timeZone;
+    @Column(name = "created_at",  nullable = false, updatable = false)
+    private Instant createdAt;
+    @Column(name = "update_at",   nullable = false)
     private Instant updatedAt;
 
 }
