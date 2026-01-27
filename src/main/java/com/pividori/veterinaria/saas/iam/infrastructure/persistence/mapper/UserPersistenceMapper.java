4e328994534a7b8fd1bc8f6a27eb3180d038a32d
SantiagoPividori <pividorisantiago@gmail.com>
2026-01-27 03:18:17 -0300

    feat(user): added UserPersistenceMapper for pass at Entity to Domain and Domain to Entity

---
+++ src/main/java/com/pividori/veterinaria/saas/iam/infrastructure/persistence/mapper/UserPersistenceMapper.java
@@ -0,0 +1,19 @@
+package com.pividori.veterinaria.saas.iam.infrastructure.persistence.mapper;
+
+import com.pividori.veterinaria.saas.iam.domain.model.User;
+import com.pividori.veterinaria.saas.iam.infrastructure.persistence.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserPersistenceMapper {
+
+    @Mapping(source = "")
+    default UserEntity toUserEntity(User user) {
+        return null;
+    }
+
+    default User toDomain(UserEntity userEntity) {
+        return null;
+    }
+}
